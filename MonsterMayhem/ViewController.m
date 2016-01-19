f0a02b8c385e607783d2b1e976e63ea0848453d1
Jon Jones <jon.p.jones13@gmail.com>
2016-01-19 14:47:50 -0600

    Third step or something

--- MonsterMayhem/ViewController.m
+++ MonsterMayhem/ViewController.m
@@ -12,6 +12,8 @@
 
 @interface ViewController () <UITableViewDataSource, UITableViewDelegate>
 @property NSMutableArray *creatures;
+@property (weak, nonatomic) IBOutlet UITextField *monsterNameAdd;
+@property (weak, nonatomic) IBOutlet UITableView *monsterTableView;
 
 
 @end
@@ -25,6 +27,11 @@
     MagicalCreature *creatureOne = [[MagicalCreature alloc] init];
     MagicalCreature *creatureTwo = [[MagicalCreature alloc] init];
     MagicalCreature *creatureThree = [[MagicalCreature alloc] init];
+    
+    creatureOne.name = @"Bob";
+    creatureTwo.name = @"Judy";
+    creatureThree.name = @"Chaz";
+    
     [self.creatures addObject:creatureOne];
     [self.creatures addObject:creatureTwo];
     [self.creatures addObject:creatureThree];
@@ -34,6 +41,7 @@
 
 -(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MonsterID"];
+    cell.textLabel.text = [[self.creatures objectAtIndex:indexPath.row] name];
     return cell;
     
 }
@@ -42,6 +50,16 @@
     return self.creatures.count;
 }
 
+- (IBAction)monsterAdd:(UIButton *)sender {
+    MagicalCreature *addCreature = [[MagicalCreature alloc] init];
+    addCreature.name = self.monsterNameAdd.text;
+    [self.creatures addObject:addCreature];
+    self.monsterNameAdd.text = @"";
+    
+    [self.monsterTableView reloadData];
+    
+    
+}
 
 
 
